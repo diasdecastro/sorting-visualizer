2bd7d140e3d8ecbe11559d9767b4fe344b62374a
diasdecastro <cristian.diass@hotmail.com>
2020-06-12 12:52:21 +0200

    responsive

--- modules/renderLines.js
+++ modules/renderLines.js
@@ -9,22 +9,22 @@ export function drawLines(arr, x, y, rightPivot, leftPivot){
         line = document.createElement("div");
         line.className = "line";
         if(i === x || i === y){
-            line.style.borderLeftWidth = (0.13).toString() + "em";
+            line.style.borderLeftWidth = (0.13).toString() + "vw";
             line.style.borderLeftStyle = "solid";
             line.style.borderLeftColor = "red";
         }
         else if(i === rightPivot){
-            line.style.borderLeftWidth = (0.13).toString() + "em";
+            line.style.borderLeftWidth = (0.13).toString() + "vw";
             line.style.borderLeftStyle = "solid";
             line.style.borderLeftColor = "aqua";
         }
         else if(i === leftPivot){
-            line.style.borderLeftWidth = (0.13).toString() + "em";
+            line.style.borderLeftWidth = (0.13).toString() + "vw";
             line.style.borderLeftStyle = "solid";
             line.style.borderLeftColor = "orange";
         }
         else{
-            line.style.borderLeftWidth = (0.13).toString() + "em";
+            line.style.borderLeftWidth = (0.13).toString() + "vw";
             line.style.borderLeftStyle = "solid";
             line.style.borderLeftColor = "black";
         }        
